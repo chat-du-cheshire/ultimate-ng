11ee864a0c376c4dceafc95d41608f45eeef9562
Max Ivanov <maxlvan@yandex.ru>
2018-12-11 14:45:18 +0300

    Lesson: Form stateless component

---
+++ src/app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
@@ -0,0 +1,19 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {Passenger} from '../../models/passenger';
+
+@Component({
+  selector: 'passenger-form',
+  templateUrl: './passenger-form.component.html',
+  styleUrls: ['./passenger-form.component.scss']
+})
+export class PassengerFormComponent implements OnInit {
+
+  @Input() detail: Passenger;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/passenger-dashboard/passenger-dashboard.module.ts
+++ src/app/passenger-dashboard/passenger-dashboard.module.ts
@@ -5,9 +5,16 @@ import {PassengerDashboardComponent} from './components/passenger-dashboard/pass
 import {PassengerCountComponent} from './components/passenger-count/passenger-count.component';
 import {PassengerDetailComponent} from './components/passenger-detail/passenger-detail.component';
 import {PassengerViewerComponent} from './components/passenger-viewer/passenger-viewer.component';
+import {PassengerFormComponent} from './components/passenger-form/passenger-form.component';
 
 @NgModule({
-  declarations: [PassengerDashboardComponent, PassengerCountComponent, PassengerDetailComponent, PassengerViewerComponent],
+  declarations: [
+    PassengerDashboardComponent,
+    PassengerCountComponent,
+    PassengerDetailComponent,
+    PassengerViewerComponent,
+    PassengerFormComponent
+  ],
   imports: [
     CommonModule,
     HttpClientModule
